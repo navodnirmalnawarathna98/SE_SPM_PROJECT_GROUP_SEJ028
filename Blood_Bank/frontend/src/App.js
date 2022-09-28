15745ae31eca62eea2881733ba2c370179ffbd32
IT20651060 <it20651060@my.sliit.lk>
2022-09-28 12:43:09 +0530

    SeekerProfileUpdateUI
    Seeker profile update form with Styles

--- Blood_Bank/frontend/src/App.js
+++ Blood_Bank/frontend/src/App.js
@@ -18,9 +18,11 @@ import Seeker from "./Pages/Seeker/Seeker";
 import BloodRequest from "./Pages/Institution/bloodrequest";
 import BloodRequestTable from "./Pages/Institution/bloodreqtable";
 
+//seeeker related imported files
 import SeekerRegister from "./Pages/Seeker/SeekerRegister";
 import SeekerProfile from "./Pages/Seeker/SeekerProfile";
 import TemporyLogin from "./Pages/Seeker/temporyLogin";
+import SeekerProfileUpdate from "./Pages/Seeker/SeekerProfileUpdate";
 import Header from "./Pages/Home-Page/Header";
 
 function App() {
@@ -46,9 +48,11 @@ function App() {
         <Route path="/bloodreq" element={<BloodRequest />} />
         <Route path="/reqtable" element={<BloodRequestTable />} />
 
+        {/* Setting paths for seeker components*/}
         <Route path="/seekerRegister" element={<SeekerRegister />} />
         <Route path="/seekerProfile" element={<SeekerProfile />} />
         <Route path="/tempLogin" element={<TemporyLogin />} />
+        <Route path="/seekerProfileUpdate" element={<SeekerProfileUpdate />} />
         <Route path="/header" element={<Header />} />
       </Routes>
     </Router>
